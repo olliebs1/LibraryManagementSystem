14a6d55a4675d1a21f9abf060c79e2bcee83f774
olliebs1 <olliebenson@hotmail.co.uk>
2022-04-13 12:31:38 +0100

    Database setup
    Initial database setup completed connecting to MSSQL ready for first migration

---
+++ LibraryData/Models/Patron.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LibraryData.Models
+{
+    public class Patron
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string TelephoneNumber { get; set; }
+
+        //public virtual LibraryCard LibraryCard { get; set; }
+    }
+}
