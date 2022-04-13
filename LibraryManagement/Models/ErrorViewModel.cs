e48209521f6ebebe2e830e482110f52710c088a1
olliebs1 <olliebenson@hotmail.co.uk>
2022-04-13 10:31:00 +0100

    Initial Commit
    First Commit after setup.

---
+++ LibraryManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace LibraryManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
