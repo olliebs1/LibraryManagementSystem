690a0bcaa648a4286089798e03e8079753bdf0d6
olliebs1 <olliebenson@hotmail.co.uk>
2022-04-14 16:21:09 +0100

    LibraryAssetService
    Library Asset project created along with libraryAssetService class.

---
+++ LibraryData/ILibraryAsset.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using LibraryData.Models;
+
+namespace LibraryData
+{
+    public interface ILibraryAsset
+    {
+        IEnumerable<LibraryAsset> GetAll();
+        LibraryAsset GetById(int id);
+        void Add(LibraryAsset newAsset);
+        string GetAuthorOrDirector(int id);
+        string GetDewetIndex(int id);
+        string GetType(int id);
+        string GetTitle(int id);
+        string GetIsbn(int id);
+
+        LibraryBranch GetCurrentLocation(int id);
+
+    }
+}
