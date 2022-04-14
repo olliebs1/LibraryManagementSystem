b08efdadf58cea714cdb40eefea9a7e6d494b152
olliebs1 <olliebenson@hotmail.co.uk>
2022-04-14 13:01:20 +0100

    Models
    Entity models created and migration made.

--- LibraryData/Models/Patron.cs
+++ LibraryData/Models/Patron.cs
@@ -6,10 +6,12 @@ namespace LibraryData.Models
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
-        public string Surname { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string TelephoneNumber { get; set; }
 
-        //public virtual LibraryCard LibraryCard { get; set; }
+        public virtual LibraryCard LibraryCard { get; set; }
+        public virtual LibraryBranch HomeLibraryBranch { get; set; }
     }
 }
