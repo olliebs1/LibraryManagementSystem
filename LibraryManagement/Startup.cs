5d6fe632fd13a999b7b4de94e2d6646edff6d4ef
olliebs1 <olliebenson@hotmail.co.uk>
2022-04-19 15:41:06 +0100

    Checkout Service layer
    Initial Checkout service layer functionality added.

--- LibraryManagement/Startup.cs
+++ LibraryManagement/Startup.cs
@@ -24,6 +24,7 @@ namespace LibraryManagement
             services.AddControllersWithViews();
             services.AddSingleton(Configuration);
             services.AddScoped<ILibraryAsset, LibraryAssetService>();
+            services.AddScoped<ICheckout, CheckoutService>();
 
             services.AddDbContext<LibraryContext>(options =>
                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
