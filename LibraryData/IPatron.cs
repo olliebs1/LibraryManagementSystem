2ad9db2d7dfd35dfed0e2b920488d819e526c79b
olliebs1 <olliebenson@hotmail.co.uk>
2022-04-20 16:46:50 +0100

    Patron functionality
    Patron Service and patron constructor methods created ready to implemented into controller

---
+++ LibraryData/IPatron.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using LibraryData.Models;
+
+namespace LibraryData
+{
+    public interface IPatron
+    {
+        Patron Get(int id);
+        IEnumerable<Patron> GetAll();
+        void Add(Patron newPatron);
+
+        IEnumerable<CheckoutHistory> GetCheckoutHistory(int patronId);
+        IEnumerable<Hold> GetHolds(int patronId);
+        IEnumerable<Checkout> GetCheckouts(int patronId);
+    }
+}
---
+++ LibraryManagement/Controllers/PatronController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagement.Controllers
+{
+    public class PatronController : Controller
+    {
+        public PatronController()
+        {
+
+        }
+    }
+}
