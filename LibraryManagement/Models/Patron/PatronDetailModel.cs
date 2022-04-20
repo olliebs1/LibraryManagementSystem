e51e2bb19a54006a6ec12c8517ca02db8c417e1d
olliebs1 <olliebenson@hotmail.co.uk>
2022-04-20 16:55:26 +0100

    Patron Model
    Patron Detail Model created

---
+++ LibraryManagement/Models/Patron/PatronDetailModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using LibraryData.Models;
+
+namespace LibraryManagement.Models.Patron
+{
+    public class PatronDetailModel
+    {
+       public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int LibraryCardId { get; set; }
+        public string Address { get; set; }
+        public DateTime MemberSince { get; set; }
+        public string Telephone { get; set; }
+        public string HomeLibraryBranch { get; set; }
+        public decimal OverDueFees { get; set; }
+
+        public IEnumerable<Checkout> AssetsCheckedOut { get; set; }
+        public IEnumerable<CheckoutHistory> CheckoutHistory { get; set; }
+        public IEnumerable<Hold> Holds { get; set; }
+    }
+}
