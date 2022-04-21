d60ea817e19b398d16f61df9bea539f217de0e97
olliebs1 <olliebenson@hotmail.co.uk>
2022-04-21 13:25:11 +0100

    Library branch
    Library branch controllers started along with services and data namespace completed

---
+++ LibraryManagement/Controllers/BranchController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagement.Controllers
+{
+    public class BranchController : Controller
+    {
+        public ILibraryBranch _branch;
+        public BranchController(ILibraryBranch branch)
+        {
+            _branch = branch;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ LibraryServices/DataHelpers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using LibraryData.Models;
+
+namespace LibraryServices
+{
+    public class DataHelpers
+    {
+        public static List<string> HumaniseBusinessHours(IEnumerable<BranchHours> branchHours)
+        {
+            var hours = new List<string>();
+
+            foreach(var time in branchHours)
+            {
+                var day = HumaniseDay(time.DayOfWeek);
+                var openTime = HumaniseTime(time.OpenTime);
+                var closeTime = HumaniseTime(time.CloseTime);
+
+                var timeEntry = $"{day} {openTime} to {closeTime}";
+
+                hours.Add(timeEntry);
+            }
+
+            return hours;
+        }
+
+        public static string HumaniseDay(int number)
+        {
+            return Enum.GetName(typeof(DayOfWeek), number);
+        }
+
+        public static string HumaniseTime(int time)
+        {
+            return TimeSpan.FromHours(time).ToString("hh' : 'mm");
+        }
+    }
+}
