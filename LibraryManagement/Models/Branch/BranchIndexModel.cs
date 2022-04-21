44de21a325974f25287555a3f520a6e3a731558d
olliebs1 <olliebenson@hotmail.co.uk>
2022-04-21 13:29:53 +0100

    Branch models
    Branch details and index models created

---
+++ LibraryManagement/Models/Branch/BranchIndexModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace LibraryManagement.Models.Branch
+{
+    public class BranchIndexModel
+    {
+       public IEnumerable<BranchDetailModel> Branches { get; set; }
+    }
+}
